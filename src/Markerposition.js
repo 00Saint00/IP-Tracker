bb12434bd1dec860f2b8562248f01c602057c071
00Saint00 <74537219+00Saint00@users.noreply.github.com>
2023-12-10 22:21:20 +0100

    first commit

---
+++ src/Markerposition.js
@@ -0,0 +1,36 @@
+import { React, useEffect } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
+import icon from "./icon";
+import "./index.css";
+
+export default function Markerposition({ address }) {
+  const position = [address.location.lat, address.location.lng];
+  const map = useMap();
+
+  const popupStyle = {
+    marginLeft: "-20px", // without !important
+    backgroundColor: "red",
+  };
+
+  const popupContent = (
+    <p className="font-semibold text-slate-900 text-large md:text-xl xl:text-2xl">
+      {address.location.city}, {address.location.region}
+    </p>
+  );
+
+  useEffect(() => {
+    map.flyTo(position, 13, {
+      animate: true,
+    });
+  }, [map, position]);
+
+  return (
+    <>
+      <Marker icon={icon} position={position}>
+        <Popup style={popupStyle} className="popupCustomStyle">
+          {popupContent}
+        </Popup>
+      </Marker>
+    </>
+  );
+}
