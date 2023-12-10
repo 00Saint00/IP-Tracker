67d289f507f1cd10bd2eeff4a405f615bde7b0ab
00Saint00 <74537219+00Saint00@users.noreply.github.com>
2023-12-10 22:38:19 +0100

    fixed key

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     try {
       const getInitialData = async () => {
         const res = await fetch(
-          `https://geo.ipify.org/api/v2/country,city?apiKey=at_MxvRSmEBpijth2GOLQaaP8SGYPPYN&ipAddress=8.8.8.8`
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=8.8.8.8`
         );
         const data = await res.json();
         setAddress(data);
